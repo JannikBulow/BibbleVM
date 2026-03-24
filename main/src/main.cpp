752f31203b17f49b467953c3422804668ee230f0
JesusTouchMe <dodoakmishot@gmail.com>
2026-03-24 14:01:57 +0100

    chore: add string pool testing

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -1,8 +1,18 @@
 // Copyright 2026 Jannik Laugmand Bülow
 
+#include <BibbleVM/util/string_pool.h>
+#include <BibbleVM/init.h>
+
 #include <iostream>
 
 int main(int argc, char** argv) {
-    std::cout << "Hello World!\n";
+    bibblevm::InitDependencies();
+
+    bibblevm::StringPool pool;
+
+    bibblevm::String s1 = pool.intern("Hello World");
+
+    std::cout << s1 << "\n";
+
     return 0;
 }
