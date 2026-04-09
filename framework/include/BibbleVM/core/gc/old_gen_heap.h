6cd80e48e731d0a5996226a1fe9329c9ea2ae6b8
JesusTouchMe <dodoakmishot@gmail.com>
2026-04-09 13:38:49 +0200

    feat(memory-manager): add more to the OldGenHeap interface

--- framework/include/BibbleVM/core/gc/old_gen_heap.h
+++ framework/include/BibbleVM/core/gc/old_gen_heap.h
@@ -1,23 +1,48 @@
 // Copyright 2026 Jannik Laugmand Bülow
 
 #ifndef BIBBLEVM_CORE_GC_OLD_GEN_HEAP_H
-#define BIBBLEVM_CORE_GC_OLD_GEN_HEAP_H
+#define BIBBLEVM_CORE_GC_OLD_GEN_HEAP_H 1
 
 #include "BibbleVM/core/oop/object.h"
 
+#include "BibbleVM/util/time_manager.h"
+
 namespace bibblevm::gc {
-    class OldGenHeap {
+    class BIBBLEVM_EXPORT OldGenHeap {
     public:
+        enum class Phase {
+            Idle,
+            Mark,
+            Sweep
+        };
+
         OldGenHeap() = default;
         ~OldGenHeap();
 
-        bool init(size_t initialSize);
+        bool init(VM& vm);
+
+        size_t getCapacity() const;
+        size_t getUsed() const;
+        double getLoadFactor() const;
+
+        bool containsObject(const oop::Object* object) const;
+
+        Phase getPhase() const;
+        bool isCollecting() const;
 
         oop::Object* allocate(size_t byteSize);
 
         void grow(size_t newRegionSize);
         void shrink();
+
+        void startCollection(VM& vm);
+        bool stepCollection(VM& vm, TimeManager<>::TimePoint startTime);
+
+        void markObject(oop::Object* object);
+
+    private:
+
     };
 }
 
-#endif //BIBBLEVM_CORE_GC_OLD_GEN_HEAP_H
+#endif // BIBBLEVM_CORE_GC_OLD_GEN_HEAP_H
