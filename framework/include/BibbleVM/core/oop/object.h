d01757174cff03bbefa25175d0400a4a54286ebd
JesusTouchMe <dodoakmishot@gmail.com>
2026-04-12 01:04:31 +0200

    refactor(object): add const to all resolvedType parameters in visitChildren

--- framework/include/BibbleVM/core/oop/object.h
+++ framework/include/BibbleVM/core/oop/object.h
@@ -36,7 +36,7 @@ namespace bibblevm::oop {
         Future* asFuture() { return reinterpret_cast<Future*>(this); }
 
         // Takes its type explicitly because I want this function inline and I can't include memory_manager.
-        constexpr void visitChildren(Type* resolvedType, auto visitor);
+        constexpr void visitChildren(const Type* resolvedType, auto visitor);
     };
 
     struct Instance {
@@ -46,7 +46,7 @@ namespace bibblevm::oop {
 
         Object* asObject() { return &header; }
 
-        constexpr void visitChildren(Type* resolvedType, auto visitor) {
+        constexpr void visitChildren(const Type* resolvedType, auto visitor) {
             Object** children = reinterpret_cast<Object**>(fieldBytes);
             for (uint16_t i = 0; i < resolvedType->getObjectFieldCount(); i++) {
                 visitor(children[i]);
@@ -54,14 +54,15 @@ namespace bibblevm::oop {
         }
     };
 
-    struct Array {        Object header;
+    struct Array {
+        Object header;
 
         ULong length;
         char elementBytes[];
 
         Object* asObject() { return &header; }
 
-        constexpr void visitChildren(Type* resolvedType, auto visitor) {
+        constexpr void visitChildren(const Type* resolvedType, auto visitor) {
             if (resolvedType->baseType->isObjectType()) {
                 Object** children = reinterpret_cast<Object**>(elementBytes);
                 for (ULong i = 0; i < length; i++) {
@@ -78,7 +79,7 @@ namespace bibblevm::oop {
 
         Object* asObject() { return &header; }
 
-        constexpr void visitChildren(Type* resolvedType, auto visitor) {}
+        constexpr void visitChildren(const Type* resolvedType, auto visitor) {}
     };
 
     struct BIBBLEVM_EXPORT Future {
@@ -92,7 +93,7 @@ namespace bibblevm::oop {
 
         Object* asObject() { return &header; }
 
-        constexpr void visitChildren(Type* resolvedType, auto visitor) {
+        constexpr void visitChildren(const Type* resolvedType, auto visitor) {
             visitor(waiters);
         }
 
@@ -100,7 +101,7 @@ namespace bibblevm::oop {
         void complete(VM& vm, Value value);
     };
 
-    constexpr void Object::visitChildren(Type* resolvedType, auto visitor) {
+    constexpr void Object::visitChildren(const Type* resolvedType, auto visitor) {
         switch (resolvedType->kind) {
             case Type::Instance:
                 asInstance()->visitChildren(resolvedType, visitor);
