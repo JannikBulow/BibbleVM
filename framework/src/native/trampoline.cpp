d35060673236d1c29b2bb0cee48ff33130d150e6
JesusTouchMe <dodoakmishot@gmail.com>
2026-04-29 08:23:29 +0200

    fix(native-trampoline): add missing push and pop operations of local reference frames

--- framework/src/native/trampoline.cpp
+++ framework/src/native/trampoline.cpp
@@ -13,6 +13,8 @@ namespace bibblevm::native {
         uint16_t parameterCount = frame.getFunction().getParameterCount();
         VMValue* arguments = frame.arena()->allocate<VMValue>(parameterCount);
 
+        vm.memoryManager().pushLocalReferenceFrame(parameterCount);
+
         for (uint16_t i = 0; i < parameterCount; ++i) {
             arguments[i] = frame[i].toNative(vm.memoryManager());
         }
@@ -20,6 +22,8 @@ namespace bibblevm::native {
         void* implementation = frame.getFunction().implementation();
         VMReturnValue ret = reinterpret_cast<Signature>(implementation)(vm, &task->nativeInterface, arguments);
         Value value = Value::FromNative(ret.value, ret.reference);
+
+        vm.memoryManager().popLocalReferenceFrame();
 
         return executor::SchedulerMessage::Returned(value);
     }
