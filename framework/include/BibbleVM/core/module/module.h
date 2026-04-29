c40a1821df6e60e4feb6638af49dcf5b18d45adc
JesusTouchMe <dodoakmishot@gmail.com>
2026-04-29 13:45:31 +0200

    chore(module): switch to BibbleBytecode modules
    Simplifies reader and keeps it up to date in case I change the format.

--- framework/include/BibbleVM/core/module/module.h
+++ framework/include/BibbleVM/core/module/module.h
@@ -3,26 +3,10 @@
 #ifndef BIBBLEVM_CORE_MODULE_MODULE_H
 #define BIBBLEVM_CORE_MODULE_MODULE_H 1
 
-#include "BibbleVM/core/module/class.h"
-#include "BibbleVM/core/module/function.h"
+#include <BibbleBytecode/module/module.h>
 
 namespace bibblevm::module {
-    constexpr uint32_t MAGIC = 0xFAAC6767;
-
-    struct Module {
-        uint32_t magic;
-        uint16_t formatVersion;
-
-        ConstantIndex name;
-
-        ConstPool constPool;
-
-        uint16_t classCount;
-        uint16_t functionCount;
-
-        Class* classes;
-        Function* functions;
-    };
+    using namespace bibblebytecode;
 }
 
 #endif // BIBBLEVM_CORE_MODULE_MODULE_H
