a63d3e63d1ec744791f00e113805880af65075a4
JesusTouchMe <dodoakmishot@gmail.com>
2026-06-16 12:40:01 +0200

    feat(compilation-unit): populate compilation unit struct

--- framework/include/BibbleVM/compiler/compilation_unit.h
+++ framework/include/BibbleVM/compiler/compilation_unit.h
@@ -3,6 +3,8 @@
 #ifndef BIBBLEVM_COMPILER_COMPILATION_UNIT_H
 #define BIBBLEVM_COMPILER_COMPILATION_UNIT_H 1
 
+#include "BibbleVM/compiler/code.h"
+
 namespace bibblevm {
     enum class CompilationState {
         None,
@@ -12,7 +14,9 @@ namespace bibblevm {
     };
 
     struct CompilationUnit {
+        CompilationState state = CompilationState::None;
 
+        Code* baseline = nullptr;
     };
 }
 
