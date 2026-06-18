124b0160ecdeecfb51112d0d57a6037851235403
Jannik Laugmand Bülow <jannikbulow0@gmail.com>
2026-06-18 23:08:33 +0200

    feat(trampoline): add leave protocol translation to SchedulerMessage

--- framework/src/compiler/trampoline.cpp
+++ framework/src/compiler/trampoline.cpp
@@ -3,6 +3,10 @@
 #include "BibbleVM/compiler/abi.h"
 #include "BibbleVM/compiler/trampoline.h"
 
+#include "BibbleVM/core/error.h"
+
+#include "BibbleVM/vm.h"
+
 namespace bibblevm::compiler {
     executor::SchedulerMessage Trampoline(VM& vm, executor::Frame& frame, executor::Task* task) {
         abi::Frame abiFrame{};
@@ -16,6 +20,14 @@ namespace bibblevm::compiler {
 
         abi::bibblevm_enter(&abiFrame, &leaveRegisters);
 
-        return executor::SchedulerMessage::Yielded();
+        switch (static_cast<abi::LeaveReason>(leaveRegisters.reason)) {
+            case abi::LeaveReason::Error:
+                return executor::SchedulerMessage::Errored(static_cast<uint8_t>(leaveRegisters.exit1), reinterpret_cast<oop::StringObject*>(leaveRegisters.exit2));
+            case abi::LeaveReason::Return:
+                return executor::SchedulerMessage::Returned({.isObject = leaveRegisters.exit1 != 0, .ul = leaveRegisters.exit2});
+
+            default:
+                return executor::SchedulerMessage::Errored(Error::Type::INTERNAL_ERROR, vm.memoryManager().allocateString(vm, "unknown leave reason from compiled function")->asString());
+        }
     }
 }
