2a5ba242dedd170442f37180f8143d54f280028d
JesusTouchMe <dodoakmishot@gmail.com>
2026-06-18 13:45:00 +0200

    fix(abi): remove export for bibblevm_enter
    It's not supposed to be used outside the library anyway (and it was bugged)

--- framework/include/BibbleVM/compiler/abi.h
+++ framework/include/BibbleVM/compiler/abi.h
@@ -63,7 +63,7 @@ namespace bibblevm::compiler::abi {
     constexpr int EXIT2_REGISTER = 12
 #endif
 
-    BIBBLEVM_EXPORT void bibblevm_enter(const Frame* frame, LeaveRegisters* leave);
+    void bibblevm_enter(const Frame* frame, LeaveRegisters* leave);
 }
 }
 
