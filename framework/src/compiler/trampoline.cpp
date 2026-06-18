626403518fd9272504b4e62241657f59c50a55f4
Jannik Laugmand Bülow <jannikbulow0@gmail.com>
2026-06-18 22:46:44 +0200

    feat(trampoline): add trampoline for compiled code
    It does call just fine, just not translating the return value

--- framework/src/compiler/trampoline.cpp
+++ framework/src/compiler/trampoline.cpp
@@ -5,6 +5,17 @@
 
 namespace bibblevm::compiler {
     executor::SchedulerMessage Trampoline(VM& vm, executor::Frame& frame, executor::Task* task) {
+        abi::Frame abiFrame{};
+        abiFrame.vm = &vm;
+        abiFrame.registers = frame.getRegisters();
+        abiFrame.frame = &frame;
+        abiFrame.task = task;
+        abiFrame.code = frame.getFunction().getCompilationUnit()->baseline->mc;
+
+        abi::LeaveRegisters leaveRegisters{};
+
+        abi::bibblevm_enter(&abiFrame, &leaveRegisters);
+
         return executor::SchedulerMessage::Yielded();
     }
 }
