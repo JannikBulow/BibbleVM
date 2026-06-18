c931839376c3ff591ac96095c1542259069ea17c
JesusTouchMe <dodoakmishot@gmail.com>
2026-06-18 13:47:02 +0200

    feat(compiler): add empty trampoline function definition

---
+++ framework/src/compiler/trampoline.cpp
@@ -0,0 +1,9 @@
+// Copyright 2026 Jannik Laugmand Bülow
+
+#include "BibbleVM/compiler/abi.h"
+#include "BibbleVM/compiler/trampoline.h"
+
+namespace bibblevm::compiler {
+    executor::SchedulerMessage Trampoline(VM& vm, executor::Frame& frame, executor::Task* task) {
+    }
+}
