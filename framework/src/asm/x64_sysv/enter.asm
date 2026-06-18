07497e5ac3b644bd0e8f5d4237df3b576c2fff08
Jannik Laugmand Bülow <jannikbulow0@gmail.com>
2026-06-18 22:43:32 +0200

    feat(abi): add prologue and epilogue for bibblevm_enter on sysv

--- framework/src/asm/x64_sysv/enter.asm
+++ framework/src/asm/x64_sysv/enter.asm
@@ -1,5 +1,38 @@
-global bibblevm_enter
+default rel
+
+struc Frame
+    .vm resq 1
+    .registers resq 1
+    .frame resq 1
+    .task resq 1
+endstruc
+
+struc LeaveRegisters
+    .exit0 resq 1
+    .exit1 resq 1
+    .exit2 resq 1
+endstruc
+
 section .text
 
+global bibblevm_enter
+
+; rdi : pointer to const Frame
+; rsi : pointer to LeaveRegisters
 bibblevm_enter:
+    push rbx
+    push rbp
+    push r12
+    push r13
+    push r14
+    push r15
+
+    ; prepare and enter jit code
+
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop rbp
+    pop rbx
     ret
