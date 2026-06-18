08e013c9bf70da3cb079d373d6f589e561411ab4
JesusTouchMe <dodoakmishot@gmail.com>
2026-06-18 14:00:14 +0200

    feat(compiler): add basic implementation which prints the assembly

--- framework/src/compiler/compiler.cpp
+++ framework/src/compiler/compiler.cpp
@@ -1,5 +1,6 @@
 // Copyright 2026 Jannik Laugmand Bülow
 
+#include "BibbleVM/compiler/abi.h"
 #include "BibbleVM/compiler/compiler.h"
 
 #include <vasm/instruction/singleOperandInstruction/CallInstruction.h>
@@ -25,6 +26,30 @@
 
 namespace bibblevm::compiler {
     Code* Compiler::compile(VM& vm, executor::Function* function, CompileOptions options) {
+        executor::Instruction* instructionsBegin = function->getInstructions();
+        executor::Instruction* instructionsEnd = instructionsBegin + function->getInstructionCount();
+
+        std::vector<instruction::ValuePtr> machineInstructions;
+
+        for (executor::Instruction* inst = instructionsBegin; inst != instructionsEnd; inst++) {
+            switch (inst->opcode) {
+                case LOAD_IMM: {
+                    instruction::RegisterPtr regs = std::make_unique<instruction::Register>(abi::REGS_REGISTER, codegen::OperandSize::Quad);
+                    instruction::MemoryPtr dst = std::make_unique<instruction::Memory>(std::move(regs), inst->a * 16 + 8, nullptr, std::nullopt); // TODO: make values 8 bytes
+                    instruction::ImmediatePtr imm = std::make_unique<instruction::Immediate>(inst->imm);
+                    machineInstructions.push_back(std::make_unique<instruction::MovInstruction>(std::move(dst), std::move(imm)));
+                    break;
+                }
+
+                default:
+                    break;
+            }
+        }
+
+        for (const instruction::ValuePtr& inst : machineInstructions) {
+            inst->print(std::cout);
+        }
+
         return nullptr;
     }
 }
