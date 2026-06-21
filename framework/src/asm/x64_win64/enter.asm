00874fd8232ea205038babaeb211ee12f080d3b2
Jannik Laugmand Bülow <jannikbulow0@gmail.com>
2026-06-21 22:35:04 +0200

    feat(asm-enter): save nonvolatile xmm registers as per the win64 abi
    Sysv says that all xmms are volatile, so no need to change anything there.

--- framework/src/asm/x64_win64/enter.asm
+++ framework/src/asm/x64_win64/enter.asm
@@ -30,17 +30,40 @@ bibblevm_enter:
     push r14
     push r15
 
+    sub rsp, 160
+    movaps [rsp + 0*16], xmm6
+    movaps [rsp + 1*16], xmm7
+    movaps [rsp + 2*16], xmm8
+    movaps [rsp + 3*16], xmm9
+    movaps [rsp + 4*16], xmm10
+    movaps [rsp + 5*16], xmm11
+    movaps [rsp + 6*16], xmm12
+    movaps [rsp + 7*16], xmm13
+    movaps [rsp + 8*16], xmm14
+    movaps [rsp + 9*16], xmm15
+
     push rdx
 
     mov r14, rcx
     mov r15, [rcx + Frame.registers]
     mov r13, [rcx + Frame.resumeCheckpoint]
 
-    sub rsp, 40
+    sub rsp, 32
     call [rcx + Frame.code]
-    add rsp, 40
+    add rsp, 32
 
     pop rsi
+
+    movaps xmm6,  [rsp + 0*16]
+    movaps xmm7,  [rsp + 1*16]
+    movaps xmm8,  [rsp + 2*16]
+    movaps xmm9,  [rsp + 3*16]
+    movaps xmm10, [rsp + 4*16]
+    movaps xmm11, [rsp + 5*16]
+    movaps xmm12, [rsp + 6*16]
+    movaps xmm13, [rsp + 7*16]
+    movaps xmm14, [rsp + 8*16]
+    movaps xmm15, [rsp + 9*16]
 
     mov [rsi + LeaveRegisters.reason], rax
     mov [rsi + LeaveRegisters.checkpoint], rcx
